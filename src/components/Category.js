a99b7c6da90bc182b8e0cb022ea045dfcd84d40d
AJ <ajuan54@gmail.com>
2022-07-18 20:42:48 -0700

    Recipe App

---
+++ src/components/Category.js
@@ -0,0 +1,71 @@
+import {FaHamburger} from "react-icons/fa";
+import {GiNoodles, GiChopsticks, GiCroissant} from "react-icons/gi";
+import styled from "styled-components";
+import {NavLink} from "react-router-dom";
+
+import React from 'react'
+
+function Category() {
+  return (
+    <List>
+        <SLink to={'/cuisine/French'}>
+            <GiCroissant />
+            <h4>French</h4>
+        </SLink>
+        <SLink to={'/cuisine/Korean'}>
+            <GiChopsticks />
+            <h4>Korean</h4>
+        </SLink>
+        <SLink to={'/cuisine/Vietnamese'}>
+            <GiNoodles />
+            <h4>Vietnamese</h4>
+        </SLink>
+        <SLink to={'/cuisine/American'}>
+            <FaHamburger />
+            <h4>American</h4>
+        </SLink>
+    </List>
+  )
+}
+
+const List = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 2rem 0rem;
+`;
+
+const SLink = styled(NavLink)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 2rem;
+    text-decoration: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    width: 6rem;
+    height: 6rem;
+    curser: pointer;
+    tranform: scale(0.8);
+
+    h4{
+     color: white;
+     font-size: 0.8rem;
+    }
+    svg{
+        color: white;
+        font-size: 1.5rem;
+    }
+    &.active{
+        background: linear-gradient(to right, #f27121, #e94057);
+
+        svg{
+            color: white;
+        }
+        h4{
+            color:white;
+        }
+    }
+`
+
+export default Category;
